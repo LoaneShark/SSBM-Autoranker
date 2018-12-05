c18e735e368ca14cdc6f10f3eace20a28a456339
Santiago Loane <santiago.loane@rochester.edu>
2018-12-05 09:32:30 -0500

    WIP 1.1
    bracket wave building
    some entrant stuff

--- bin/readin.py
+++ bin/readin.py
@@ -5,7 +5,7 @@ import scipy as sp
 import json
 
 ## TODO: - Finish set data readin
-## 		 - Figure out what to do with the data
+## 		 - Figure out what to do with the data // what data do we want
 
 def readin():
 	print("Henlo")
@@ -13,20 +13,36 @@ def readin():
 # reads the match data for a given phase
 def read_sets(phases):
 	entrants = {}
+	wins = {}
+	losses = {}
+	results = {}
+	pools = {}
+	bracket = {}
+
 	for phase in phases:
 		data = pull_phase(phase)
-	seedata = data['entities']['seeds']
+		seedata = data['entities']['seeds']
 
-	## PICK UP HERE
-	for x in seedata:
-		e_id = x['entrantId']
-		entrants[e_id] = (x['mutations']['entrants'][str(e_id)]['name'],x['mutations']['entrants'][str(e_id)]['playerIds'][str(x['mutations']['entrants'][str(e_id)]['participantIds'][0])])
+		for x in seedata:
+			e_id = x['entrantId']
+			res = x['placement']
+			if x['isDisqualified']:
+				res = -1
+
+			flatten = lambda l: [item for sublist in l for item in sublist]
+			if e_id in bracket:
+				
+			else:
+				bracket[e_id] = (,[phase,x['progressingPhaseGroupId']])
+
+			entrants[e_id] = (x['mutations']['entrants'][str(e_id)]['name'],x['mutations']['entrants'][str(e_id)]['playerIds'][str(x['mutations']['entrants'][str(e_id)]['participantIds'][0])])
 
 	print(entrants)
-	return entrants
+	return entrants,wins,losses,results
 
 # reads the phase data for a given tournament
-def read_phases(tourney):
+def read_phases(tourney,getBracket=False):
+	waves = {}
 	#with open(filepath) as f:
 	#	data = json.loads(f.read())
 
@@ -40,11 +56,17 @@ def read_phases(tourney):
 	phase_ids = [phase['id'] for phase in tdata['entities']['phase'] if phase['eventId'] in event_ids]
 	group_ids = [group['id'] for group in tdata['entities']['groups'] if group['phaseId'] in phase_ids]
 
+	for w in tdata['entities']['groups']:
+		if w['id'] not in waves:
+			waves[w['id']] = [w['phaseId'],w['winnersTargetPhaseId'],w['losersTargetPhaseId']]
 	#print(t_id)
 	#print(event_ids)
 	#print(phase_ids)
 	#print(group_ids)
-	return (t_id,group_ids)
+	if getBracket:
+		return (t_id,group_ids,waves)
+	else:
+		return (t_id,group_ids)
 
 # returns the full JSON data for a phase given its ID number
 def pull_phase(num):
