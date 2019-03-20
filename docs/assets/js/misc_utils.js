54ef97d47e2b279af7d9185658579ce9aab1b319
Santiago Loane <santiago.loane@rochester.edu>
2019-03-20 13:50:19 -0400

    v0.1.1
    - added search bar for player lookups
        - search bar autocompletes and selecting a name loads the player profile for their corresponding player id
        - changes the finch route and updates the uri in browser too
         - currently only loads top 50
    - actual bootstrap nav bar
    - data tables are now top 20 -- will expand to more for actual release ofc
    - imported game icons (from ssbwiki)
    - added current game icon to jumbotron corner
    - player breakdown links to other games if the player is present in their dbs
    - restructured site a bit, player_breakdown is now a layout that dynamically switches game db by liquid information
    - added player breakdown pages for all games

--- docs/assets/js/misc_utils.js
+++ docs/assets/js/misc_utils.js
@@ -21,13 +21,108 @@ function getGameId(gameTitle){
 	}
 }
 
-function snapshotToArray(snapshot) {
+function getGameTitle(gameId){
+	switch(gameId) {
+		case 1:
+			return 'Melee';
+			break;
+		case 2:
+			return 'PM';
+			break;
+		case 3:
+			return 'Smash4';
+			break;
+		case 4:
+			return '64';
+			break;
+		case 5:
+			return 'Brawl';
+			break;
+		case 1386:
+			return 'Ultimate';
+			break;
+	}
+}
+
+function getGameIconPath(gameId){
+	switch(gameId) {
+		case 1:
+			return "/assets/images/game_icons/36px-SSBM_Icon.png"
+			break;
+		case 2:
+			return "/assets/images/game_icons/36px-PM_Icon.png"
+			break;
+		case 3:
+			return "/assets/images/game_icons/36px-SSB4_Icon.png"
+			break;
+		case 4:
+			return "/assets/images/game_icons/36px-SSB64_Icon.png"
+			break;
+		case 5:
+			return "/assets/images/game_icons/36px-SSBB_Icon.png"
+			break;
+		case 1386:
+			return "/assets/images/game_icons/36px-SSBU_Icon.png"
+			break;
+	}
+}
+
+function snapshotToArray(snapshot,attr="val") {
     var returnArr = [];
     snapshot.forEach(function(childSnapshot) {
-        var item = childSnapshot.val();
-        item.key = childSnapshot.key;
-
-        returnArr.push([item]);
+    	if (attr == "val") {
+	        var item = childSnapshot.val();
+		} else if (attr == "key") {
+        	var item = childSnapshot.key;
+        } else if (attr == "both") {
+        	var item = childSnapshot.val();
+        	item.key = childSnapshot.key;
+        }
+        returnArr.push(item);
     });
     return returnArr;
+}
+
+function otherGameActivity(p_id,game_id){
+	var gameIds = [1,2,3,4,5,1386];
+	var gamePromises = [];
+	var otherGames = [];
+	for (i=0;i<gameIds.length;i++){
+		if (gameIds[i] != game_id){
+			var refStr = "/" + gameIds[i] + "_2018_1/p_info/" + p_id + "/";
+			var currGameId = gameIds[i];
+
+			var gameRef = firebase.database().ref(refStr);
+			var gamePromise = gameRef.once('value').then(function(GameSnapshot){
+				return GameSnapshot.exists();
+			});
+			gamePromises.push(gamePromise);
+		} else {
+			gamePromises.push(true);
+		}
+	}
+	
+	var otherGamesPromise = Promise.all(gamePromises).then(function(results){
+		//console.log(results);
+		for (j=0;j<gameIds.length;j++){
+			if(results[j]){
+				otherGames.push(gameIds[j]);
+			}
+		}
+		return otherGames;
+	});
+
+	return otherGamesPromise;
+}
+
+function snapshotToSearchbar(snapshot){
+	var returnArr = [];
+	snapshot.forEach(function(childSnapshot) {
+		var player = {name: childSnapshot.child('tag').val(),
+					  id: childSnapshot.key,
+					  aliases: childSnapshot.child('aliases').val()};
+
+		returnArr.push(player)
+	});
+	return returnArr;
 }
