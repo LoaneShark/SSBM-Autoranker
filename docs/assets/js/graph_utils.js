8a354f5ec691d1171fd6d42c8b86ba65912f38e5
Santiago Loane <santiago.loane@rochester.edu>
2019-03-28 13:17:38 -0400

    v0.2
    - event cards display event banner image as card header
    - skill graphs show initial values as dotted lines
    - skill graphs show dotted lines from last data point to today
    - s-rank "ranking period" before min activity is met is now a dashed line
    - fixed a lot of tooltips and such to make charts nicer
    - game pages have buttons as directory to: ranks, players, characters [wip], and events [wip]
    - added icons and icon support, necessary scripts/css
    - added country flags to region boxes on player profile, necessary scripts/css
    - changed db references to "current" db -- most have been rebuilt/uploaded
    - small hotfixes

--- docs/assets/js/graph_utils.js
+++ docs/assets/js/graph_utils.js
@@ -1,8 +1,37 @@
+// Custom plugin to only render tooltips for certain datasets (so as to not render for initialSkillset)
+Chart.plugins.register({
+  // need to manipulate tooltip visibility before its drawn (but after update)
+  beforeDraw: function(chartInstance, easing) {
+    // check and see if the plugin is active (its active if the option exists)
+    if (chartInstance.config.options.tooltips.onlyShowForDatasetIndex) {
+      // get the plugin configuration
+      var tooltipsToDisplay = chartInstance.config.options.tooltips.onlyShowForDatasetIndex;
+
+      // get the active tooltip (if there is one)
+      var active = chartInstance.tooltip._active || [];
+
+      // only manipulate the tooltip if its just about to be drawn
+      if (active.length > 0) {
+        // first check if the tooltip relates to a dataset index we don't want to show
+        if (tooltipsToDisplay.indexOf(active[0]._datasetIndex) === -1) {
+          // we don't want to show this tooltip so set it's opacity back to 0
+          // which causes the tooltip draw method to do nothing
+          chartInstance.tooltip._model.opacity = 0;
+        }
+      }
+    }
+  }
+});
+
+
 function skillChart(skillHistory,tourneySnapshot,placementSnapshot,gameId,type='elo'){
 	var ctx = $('#'+type+'-chart')[0].getContext('2d');
 	//var ctx = $('#tempChart');
 
 	var skillset = [];
+	var initialSkillset = [];
+	var rankPeriod = [];
+	var postPeriod = [];
 	if (type == 'mainrank'){
 		var skillKeys = Object.keys(skillHistory)
 		//skillKeys = Object.keys(skillHistory[skillYear])
@@ -26,10 +55,7 @@ function skillChart(skillHistory,tourneySnapshot,placementSnapshot,gameId,type='
 			var t_name = tourneySnapshot.child(t_id.toString()).child('name').val();
 			var jsDate = new Date(t_date[0],t_date[1]-1,t_date[2]);
 
-			//console.log(t_id);
-			//console.log(tourneySnapshot.child(t_id.toString()).child('name').val());
-			//console.log(jsDate);
-			//console.log(skillval);
+			// add to skillset
 			skillset.push({'t': jsDate, 'y': skillval, 'label':t_name});
 		});
 	}
@@ -37,7 +63,39 @@ function skillChart(skillHistory,tourneySnapshot,placementSnapshot,gameId,type='
 		return a.t-b.t;
 	})
 
+
 	var legendColors = chartColorsFromType(type);
+	var todayDate = new Date();
+	firstEvent = skillset[0]
+	lastEvent = skillset[skillset.length-1]
+	var initialDate = new Date(firstEvent.t.getFullYear(),Math.max(firstEvent.t.getMonth()-1,0),1);
+	var yBounds = getYAxisBounds(gameId,type);
+	// fill in "skill" before being placed,ranked (dashed line)
+	if (type == 'srank'){
+		initialSkillset.push({'t':initialDate,'y':1.0,'label':''});
+		initialSkillset.push({'t':firstEvent.t,'y':firstEvent.y,'label':''});
+	} else if (type == 'elo' || type == 'glicko' || type == 'trueskill'){
+		initialSkillset.push({'t':initialDate,'y':1500,'label':''})
+		initialSkillset.push({'t':firstEvent.t,'y':firstEvent.y,'label':''});
+	}
+	initialSkillset.sort(function(a,b){
+		return a.t-b.t;
+	})
+	// add a dotted line from their last event to today's date
+	postPeriod.push({'t':lastEvent.t,'y':lastEvent.y,'label':''})
+	postPeriod.push({'t':todayDate,'y':postPeriod[0].y,'label':''})
+	// separate out the period before being ranked from the rest of the sets
+	if (type == 'srank'){
+		rankLen = Math.min(3,skillset.length);
+		for (i=0;i<rankLen;i++){
+			if (i<rankLen-1){
+				rankEvent = skillset.shift()
+				rankPeriod.push({'t':rankEvent.t,'y':rankEvent.y,'label':rankEvent.label});
+			} else {
+				rankPeriod.push({'t':skillset[0].t,'y':skillset[0].y,'label':skillset[0].label});
+			}
+		}
+	}
 
 	var myChart = new Chart(ctx, {
 		type: 'line',
@@ -49,19 +107,59 @@ function skillChart(skillHistory,tourneySnapshot,placementSnapshot,gameId,type='
 				steppedLine: true,
 				fill: false,
 				data: skillset
+			},{
+				label: chartLabelFromType(type,gameId)+' (Inferred)',
+				backgroundColor: legendColors[1],
+				borderColor: legendColors[0],
+				steppedLine: true,
+				pointRadius: 0,
+				pointHoverRadius: 0,
+				fill: false,
+				borderDash: [5,5],
+				data: initialSkillset
+			},{
+				label: chartLabelFromType(type,gameId)+' (Current)',
+				backgroundColor: legendColors[1],
+				borderColor: legendColors[0],
+				steppedLine: true,
+				pointRadius: 0,
+				pointHoverRadius: 0,
+				fill: false,
+				borderDash: [5,5],
+				data: postPeriod
+			},{
+				label: chartLabelFromType(type,gameId)+' (Unranked)',
+				backgroundColor: legendColors[1],
+				borderColor: legendColors[0],
+				steppedLine: true,
+				fill: false,
+				borderDash: [5,5],
+				data: rankPeriod
 			}]
 		},
 		options: {
 			responsive: true,
-			//legend: false,
+			legend: false,
+			title: {
+				display: true,
+				text: chartLabelFromType(type,gameId)
+			},
 			elements: {
 				line: {
 					tension: 0
 				}
 			},
-			tooltips : {
+			hover: {
+				mode: 'x'
+			},
+			tooltips: {
+				onlyShowForDatasetIndex: [0,3],
 				displayColors: false,
 				callbacks: {
+					title: function(tooltipItem,data){
+						var eventDate = new Date(tooltipItem[0].xLabel)
+						return eventDate.toDateString();
+					},
 					beforeLabel: function(tooltipItem,data){
 						var eventName = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].label;
 						var currSkill = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].y;
@@ -73,30 +171,13 @@ function skillChart(skillHistory,tourneySnapshot,placementSnapshot,gameId,type='
 						return eventName+"\n"+currSkill;
 					},
 					label: function(tooltipItem,data){
+						//console.log(tooltipItem)
 						if (tooltipItem.index == 0){
 							if (type == 'srank'){
 								var oldSkill = 1.0;
 							} else if (type == 'mainrank'){
-								switch (gameId){
-									case 1:
-										var oldSkill = 100;
-										break;
-									case 2:
-										var oldSkill = 50;
-										break;
-									case 3:
-										var oldSkill = 50;
-										break;
-									case 4:
-										var oldSkill = 64;
-										break;
-									case 5:
-										var oldSkill = 100;
-										break;
-									case 1386:
-										var oldSkill = 50;
-										break;
-								}
+								var oldSkill = getMainrankSize(gameId);
+								return 'NEW!'
 							} else {
 								var oldSkill = 1500.0;
 							}
@@ -121,14 +202,24 @@ function skillChart(skillHistory,tourneySnapshot,placementSnapshot,gameId,type='
 					},
 					labelTextColor: function(tooltipItem,data){
 						if (tooltipItem.index == 0){
-							var oldSkill = 1500.0;
+							if (type == 'mainrank'){
+								var oldSkill = 999999;
+							}else if (type == 'srank'){
+								var oldSkill = 1;
+							} else {
+								var oldSkill = 1500.0;
+							}
 						} else {
 							var oldSkill = data.data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index-1].y;
 						}
-						var skillDel = Math.round(data.data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].y - oldSkill);
-						if (type == 'mainrank' || type == 'srank'){
+						var skillDel = data.data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].y - oldSkill;
+						if (type == 'mainrank'){
 							skillDel = -skillDel;
 						}
+						if (type == 'srank'){
+							skillDel = -100.0*skillDel;
+						}
+						skillDel = Math.round(skillDel)
 						if (skillDel > 0){
 							return 'rgb(96,242,96)';
 						} else if (skillDel < 0) {
@@ -145,8 +236,17 @@ function skillChart(skillHistory,tourneySnapshot,placementSnapshot,gameId,type='
 	            xAxes: [{
 	                type: 'time',
 	                time: {
-	                    unit: 'month'
+	                    unit: 'month',
+	                    min: initialDate,
+	                    max: todayDate
 	                }
+	            }],
+	            yAxes: [{
+	            	type: 'linear',
+	            	ticks: {
+	            		suggestedMin: yBounds[0],
+	            		suggestedMax: yBounds[1]
+	            	}
 	            }]
 	        }
 	    }
@@ -212,7 +312,7 @@ function chartColorsFromType(type='elo'){
 			return ['rgb(30, 173, 234)','rgb(40, 183, 244)'];
 			break;
 		case 'elo':
-			return ['rgb(164,247,204)','rgb(174,255,214)'];
+			return ['rgb(144,227,184)','rgb(154,247,194)'];
 			break;
 		case 'glicko':
 			return ['rgb(242, 96, 96)','rgb(252, 106, 106)'];
@@ -224,4 +324,43 @@ function chartColorsFromType(type='elo'){
 			return ['rgb(139, 55, 229)','rgb(149, 65, 239)'];
 			break;
 	}
+}
+
+function getMainrankSize(gameId){
+	switch (gameId){
+		case 1:
+			return 100;
+			break;
+		case 2:
+			return 50;
+			break;
+		case 3:
+			return 50;
+			break;
+		case 4:
+			return 64;
+			break;
+		case 5:
+			return 100;
+			break;
+		case 1386:
+			return 50;
+			break;
+	}
+
+}
+
+function getYAxisBounds(gameId,skillType){
+	if (skillType == 'mainrank'){
+		var minVal = 0;
+		var maxVal = getMainrankSize(gameId)+1;
+	} else if (skillType == 'srank'){
+		var minVal = -0.05
+		var maxVal = 1.05
+	} else {
+		var minVal = 1000
+		var maxVal = 2500
+	}
+
+	return [minVal,maxVal];
 }
